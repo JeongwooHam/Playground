9670f463044c52b7b79bec5301fae03432871eee
JeongwooHam <hamjw0122@gmail.com>
2024-04-05 15:44:19 +0900

    ✅ test: storybook interaction test 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
   ],
   core: {
     builder: "@storybook/builder-vite",
